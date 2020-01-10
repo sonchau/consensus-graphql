9165a06406888dc2f7f5fe6fde6293186a1e8a7a
“Son <son.chau@geografia.com.au>
2020-01-10 15:25:04 +1100

    update port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import initializeApp from './initialize-app';
 
-//const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3000;
 const PORT = 3001;
 
 initializeApp()
   .then(app => {
-    app.listen(PORT, (err: Error) => {
-      if (err) throw err;
-      console.log(`Ready on port ${PORT}`);
-    });
+    app.listen({ port: process.env.PORT || PORT }, () => {
+      console.log('server is up')
+    })
   })
   .catch(e => {
     console.error('Failed to start', e);
