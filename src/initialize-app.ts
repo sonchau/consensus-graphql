aaf5da9c45e0cc0a792a9435ea01f063da258d31
“Son <son.chau@geografia.com.au>
2020-01-13 10:49:33 +1100

    [#2] allow cors

--- src/initialize-app.ts
+++ src/initialize-app.ts
@@ -17,7 +17,7 @@ export default async () => {
       playground: true,
     }
   );
-  server.applyMiddleware({ app, cors: { origin: ['http://localhost:3000'] } });
+  server.applyMiddleware({ app, cors: { origin: ['https://consensus-geografia.herokuapp.com/'] } });
 
   return app;
 };
