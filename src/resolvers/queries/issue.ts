311f440f9fa9406a30f0e2707968a28846ffe874
“Son <son.chau@geografia.com.au>
2020-04-15 10:38:12 +1000

    [#3] update issue repo query

--- src/resolvers/queries/issue.ts
+++ src/resolvers/queries/issue.ts
@@ -2,7 +2,7 @@ import { getRepository } from 'typeorm';
 import { Issue } from '../../entities/issue';
 
 export default async (_root: any, { id }: { id: number }) => {
-  const taskRepo = getRepository(Issue);
-  const task = await taskRepo.findOne({ where: { id } });
-  return task;
+  const issueRepo = getRepository(Issue);
+  const issue = await issueRepo.findOne({ where: { id } });
+  return issue;
 };
