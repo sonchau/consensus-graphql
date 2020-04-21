b885a3de22a088243ab1a830610258efdc9d5567
“Son <son.chau@geografia.com.au>
2020-04-21 14:58:43 +1000

    [#4] criteriaByIssueId now returns issueId in the response

--- src/resolvers/queries/criteriasByIssueId.ts
+++ src/resolvers/queries/criteriasByIssueId.ts
@@ -3,7 +3,7 @@ import { Criteria } from '../../entities/criteria';
 
 export default async (_root: any, { issueId }: { issueId: number }) => {
   const criteriaRepo = getRepository(Criteria);
-  const criterias = await criteriaRepo.find({ issue: { id : issueId } });
+  const criterias = await criteriaRepo.find({ where: { issue: { id : issueId } }, relations: ["issue"]});
   //console.log('criterias', criterias, 'issueId', issueId)
   return criterias;
 };
